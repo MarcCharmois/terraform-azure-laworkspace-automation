e6486ac28a742c032dff5e53045b0262ff7e1818
Marc Charmois <marc.charmois@hotmail.com>
2019-10-10 18:07:40 +0200

    adding variable for resource group name

--- loganalytics-automation.tf
+++ loganalytics-automation.tf
@@ -7,18 +7,18 @@ data "azurerm_subscription" "current" {
 
 }
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = "${var.resourceGroupName}"
   location = "francecentral"
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "${var.prefix}-laworkspace"
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.prefix}-laworkspace-${var.subscription_Id}"
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   sku                 = "PerGB2018"
 }
 
-resource "azurerm_automation_account" "example" {
+resource "azurerm_automation_account" "automation" {
   name                = "${var.prefix}-automationAccount1"
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
@@ -32,16 +32,16 @@ resource "azurerm_automation_account" "example" {
 
 resource "azurerm_automation_variable_string" "law-id" {
   name                    = "la-workspaceId"
-  automation_account_name = "${azurerm_automation_account.example.name}"
+  automation_account_name = "${azurerm_automation_account.automation.name}"
   resource_group_name     = "${azurerm_resource_group.rg.name}"
-  value                   = "${azurerm_log_analytics_workspace.example.workspace_id}"
+  value                   = "${azurerm_log_analytics_workspace.law.workspace_id}"
   encrypted               = "false"
 }
 
 resource "azurerm_automation_variable_string" "law-key" {
   name                    = "la-workspaceKey"
-  automation_account_name = "${azurerm_automation_account.example.name}"
+  automation_account_name = "${azurerm_automation_account.automation.name}"
   resource_group_name     = "${azurerm_resource_group.rg.name}"
-  value                   = "${azurerm_log_analytics_workspace.example.primary_shared_key}"
+  value                   = "${azurerm_log_analytics_workspace.law.primary_shared_key}"
   encrypted               = "true"
 }
--- variables.tf
+++ variables.tf
@@ -2,6 +2,9 @@ variable "subscription_Id" {
   description = "Id of the subscription where you want to create resources"
 }
 
+variable "resourceGroupName" {
+  description = "The Azure resource group in which all resources in this example should be created."
+}
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
@@ -9,3 +12,5 @@ variable "prefix" {
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
+
+
