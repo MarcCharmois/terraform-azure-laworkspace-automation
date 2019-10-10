3d70e941d60e4a74bef2e4cf57da9a79b4b6fdbb
Marc Charmois <marc.charmois@hotmail.com>
2019-10-10 16:35:49 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "subscription_Id" {
+  description = "Id of the subscription where you want to create resources"
+}
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
