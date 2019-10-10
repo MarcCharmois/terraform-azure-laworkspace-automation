8a3ba62cfa96c323c95018b7e2d153b12dea0997
Marc Charmois <marc.charmois@hotmail.com>
2019-10-10 16:42:11 +0200

    code formatting

--- loganalytics-automation.tf
+++ loganalytics-automation.tf
@@ -1,15 +1,14 @@
 provider "azurerm" {
-    version = "~>1.27"
-    subscription_id="${var.subscription_Id}"
+  version         = "~>1.27"
+  subscription_id = "${var.subscription_Id}"
 }
-
 
 data "azurerm_subscription" "current" {
-    
+
 }
 resource "azurerm_resource_group" "rg" {
-    name     = "myTFResourceGroup"
-    location = "francecentral"
+  name     = "myTFResourceGroup"
+  location = "francecentral"
 }
 
 resource "azurerm_log_analytics_workspace" "example" {
@@ -31,19 +30,18 @@ resource "azurerm_automation_account" "example" {
   }
 }
 
-
 resource "azurerm_automation_variable_string" "law-id" {
-  name = "la-workspaceId"
+  name                    = "la-workspaceId"
   automation_account_name = "${azurerm_automation_account.example.name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  value = "${azurerm_log_analytics_workspace.example.workspace_id}"
-  encrypted="false"
+  resource_group_name     = "${azurerm_resource_group.rg.name}"
+  value                   = "${azurerm_log_analytics_workspace.example.workspace_id}"
+  encrypted               = "false"
 }
 
 resource "azurerm_automation_variable_string" "law-key" {
-  name = "la-workspaceKey"
+  name                    = "la-workspaceKey"
   automation_account_name = "${azurerm_automation_account.example.name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  value = "${azurerm_log_analytics_workspace.example.primary_shared_key}"
-  encrypted="true"
+  resource_group_name     = "${azurerm_resource_group.rg.name}"
+  value                   = "${azurerm_log_analytics_workspace.example.primary_shared_key}"
+  encrypted               = "true"
 }
